6d59e5900eb6e2a2f492cb0d69420dcd99514391
Daniel Ortega <dortega1016@gmail.com>
2026-09-10 16:03:21 -0500

    feat: add multiplication calcultate

--- calculator.py
+++ calculator.py
@@ -8,6 +8,7 @@ def calculate():
     print("Welcome to the Pair Calculator!")
     print("Addition: 5 + 3 =", add(5, 3))
     print("Subtraction: 5 - 3 =", subtract(5, 3))
+    print("Multiplication: 5 * 3 -", multiply(5, 3))
 
 def multiply(a, b):
     return a * b
